74c633dfbd772cfc917c2bca0757433fce68362c
Loka Kanarp <lokakanarp@gmail.com>
2020-06-13 15:10:10 +0200

    Changing users router to POST and adding express validation.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/users
-// @desc Test route
+// @route POST api/users
+// @desc Register user
 // @acess Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/',
+[
+  check('name', 'Name is required.').not().isEmpty(),
+  check('email', 'Please include a valid email!').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 5 })
+], (req, res) => {
+  console.log(req.body);
+  res.send('User route');
+});
 
 module.exports = router;
