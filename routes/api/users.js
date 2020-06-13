e9bb77323d738062532c858def0ad2763f9a4b5e
Loka Kanarp <lokakanarp@gmail.com>
2020-06-13 17:24:53 +0200

    Hashing password and saving user.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -25,15 +27,31 @@ async (req, res) => {
     //See if user exists
     let user = await User.findOne({ email });
     if (user) {
-      res.status(400).json({ errors: [{msg: 'User already exists.'}]});
+      return res.status(400).json({ errors: [{msg: 'User already exists.'}]});
     }
     // Get users gravatar
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
 
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
     //Encrypt password
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
 
     // Return jsonwebtoken
 
-    res.send('User route');
+    res.send('User registred');
   } catch(err) {
     console.console.error(err.message);
     res.status(500).send('Server error');
