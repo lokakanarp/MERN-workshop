8da5901acf2d6db77c27203901020ac02ceedad2
Loka Kanarp <lokakanarp@gmail.com>
2020-06-13 15:44:31 +0200

    Adding function to see if user exists.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
+
 // @route POST api/users
 // @desc Register user
 // @acess Public
@@ -9,10 +11,34 @@ router.post('/',
 [
   check('name', 'Name is required.').not().isEmpty(),
   check('email', 'Please include a valid email!').isEmail(),
-  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 5 })
-], (req, res) => {
-  console.log(req.body);
-  res.send('User route');
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+],
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+    //See if user exists
+    let user = await User.findOne({ email });
+    if (user) {
+      res.status(400).json({ errors: [{msg: 'User already exists.'}]});
+    }
+    // Get users gravatar
+
+    //Encrypt password
+
+    // Return jsonwebtoken
+
+    res.send('User route');
+  } catch(err) {
+    console.console.error(err.message);
+    res.status(500).send('Server error');
+  }
+
 });
 
 module.exports = router;
