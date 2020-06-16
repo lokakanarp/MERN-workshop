531067daa8549a9a5ef1b8ad9a92f0d19665d5a0
Loka Kanarp <lokakanarp@gmail.com>
2020-06-16 11:11:49 +0200

    Creating new profile object to send to db.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -59,11 +59,35 @@ router.get('/me', auth, async (req, res) => {
   if(status) profileFields.status = status;
   if(githubusername) profileFields.githubusername = githubusername;
   if(skills) {
-    console.log(123);
     profileFields.skills = skills.split(',').map(skill => skill.trim());
   }
-  console.log(profileFields);
-  res.send('Hello');
+  // Build social Object
+  profileFields.social = {}
+  if (youtube) profileFields.social.youtube = youtube;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id }); //Mongoose method returns a promise.
+    if(profile) {
+      //Update
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true });
+        return res.json(profile);
+    }
+    // Create
+    profile = new Profile(profileFields);
+    await Profile.save();
+    res.json(profile);
+    
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 
